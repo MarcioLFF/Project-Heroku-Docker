ddbd696296981fc3adbe5124b24b1781194269ff
Marcio <marciofonseca.ufrj@gmail.com>
2022-05-10 02:17:04 -0300

    requisito 1

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const PORT = process.env.SERVER_PORT || null;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
